d306739a80571a57769a7b148333541bfcc8546f
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-04-03 20:07:18 +0500

    import hm json data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Title from '../Title/Title';
+import Table from '../Table/Table';
 import './App.css';
+import data from '../../data/half-marathon.json';
 
 class App extends Component {
   render() {
+    const { hm2 } = data;
+
     return (
-      <div className="App">
+      <div className="app">
+        <Title component={'h2'} text={hm2.title} />
+        <Table data={hm2} />
       </div>
     );
   }
