a036c0a61a9eec3f007270f79653c89dddd667e6
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-04-03 11:11:32 +0500

    add title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Title.css';
+
+class Title extends Component {
+  render() {
+    return (
+      <h1 className="title">
+        Half Marathon Schedule 2: 73 to 102 km per week
+      </h1>
+    );
+  }
+}
+
+export default Title;
