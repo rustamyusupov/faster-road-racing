3d965f9933588dd6caa2cf0f1b03a59cd0eb7bb1
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-04-05 19:23:58 +0500

    remove double quotes in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div className="app__title">
+        <div className='app__title'>
           <Title size='big' align='center' data='Faster Road Racing' />
         </div>
 
