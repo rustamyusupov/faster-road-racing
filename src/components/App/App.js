ba95e010b73052faea059adbd1b953b29a2cd621
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-04-05 19:19:11 +0500

    add app title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Title from '../Title/Title';
 import Schedule from '../Schedule/Schedule';
 import data from '../../data/half-marathon2.json';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className='app'>
+        <div className="app__title">
+          <Title size='big' align='center' data='Faster Road Racing' />
+        </div>
+
         <Schedule plan={data} />
       </div>
     );
