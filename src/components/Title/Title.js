b4f6419e99edc682ad602eec7b21d2bf65be30a6
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-04-05 19:18:45 +0500

    add align and styles for title

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,14 +4,15 @@ import './Title.css';
 
 class Title extends Component {
   render() {
-    const { component, size } = this.props;
+    const { component, size, align } = this.props;
     const Tag = component || 'h1';
 
     return (
       <Tag
         className={cx({
           title: true,
-          [`title_size_${size}`]: size
+          [`title_size_${size}`]: size,
+          [`title_align_${align}`]: align
         })}
       >
         {this.props.data}
