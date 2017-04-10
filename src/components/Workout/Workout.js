ce504ba03bc315ea1e804d17390f84c341b2f201
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-04-10 11:16:54 +0500

    add modal

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './Workout.css';
 import Button from '../Button/Button';
 import Tooltip from '../Tooltip/Tooltip';
+import Modal from '../Modal/Modal';
 
 class Workout extends Component {
   state = {
     tooltip: false,
+    showModal: false
   }
 
   handleMouseOver = () => {
@@ -16,21 +18,34 @@ class Workout extends Component {
     this.setState({ tooltip: false });
   }
 
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  }
+
   renderType(type) {
     const { abbr } = this.props;
     const description = abbr[type] || '';
 
     return (
       <span className='workout__type'>
-        <span 
-          className='workout__link'
+        <Modal
+          contentLabel='Test'
+          close='true'
+          isOpen={this.state.showModal}
+          onRequestClose={this.handleCloseModal}
+        >
+        </Modal>
+        <Button type='link'
+          onClick={this.handleOpenModal}
           onMouseOver={this.handleMouseOver}
           onMouseOut={this.handleMouseOut}
         >
-          <Button type='link'>
-            {type}
-          </Button>
-        </span>
+          {type}
+        </Button>
 
         <span className='workout__tooltip'>
           <Tooltip visible={this.state.tooltip}>
